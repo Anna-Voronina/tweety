72cc0bc95d31a0a48ecfdf926ce7b6c163c80b19
Anna-Voronina <anna.vs2512@gmail.com>
2023-07-28 16:50:34 +0300

    ADDED: API requests

---
+++ src/utils/users-api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const BASE_URL = "https://64ac30839edb4181202f410a.mockapi.io";
+
+const usersAPI = axios.create({ baseURL: BASE_URL });
+
+export const fetchAllUsers = async () => {
+  const response = await usersAPI.get("/users");
+  return response.data;
+};
+
+export const updateFollowers = async ({ id, updatedFollowers, isFollowed }) => {
+  const response = await usersAPI.put(`/users/${id}`, {
+    followers: updatedFollowers,
+    isFollowed,
+  });
+  return response;
+};
